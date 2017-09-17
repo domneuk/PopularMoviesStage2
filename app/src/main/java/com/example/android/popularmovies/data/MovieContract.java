84454dd100cab9d684fbc686576b52f3cb7540aa
Dominik Neukamm <dominik.neukamm@mbconnectline.de>
2017-09-17 22:26:55 +0200

    typo: MovieContract

--- app/src/main/java/com/example/android/popularmovies/data/MovieContract.java
+++ app/src/main/java/com/example/android/popularmovies/data/MovieContract.java
@@ -6,17 +6,17 @@ import android.provider.BaseColumns;
 public class MovieContract {
 
     public static final String CONTENT_AUTHORITY = "com.example.android.popularmovies";
-    public static final Uri BASE_CONTENT_RUL = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     public static final String PATH_MOVIES = "movies";
 
     public static class MovieEntry implements BaseColumns {
 
-        public static final Uri CONTENT_URL = BASE_CONTENT_RUL.buildUpon()
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_MOVIES).build();
 
         public static final String TABLE_NAME = "movies";
 
-        public static final String COLUMN_MOVIE_ID = "id";
+        public static final String COLUMN_MOVIE_ID = "movie_id";
         public static final String COLUMN_MOVIE_TITLE = "title";
         public static final String COLUMN_MOVIE_USER_RATING = "rating";
         public static final String COLUMN_MOVIE_RELEASE_DATE = "date";
