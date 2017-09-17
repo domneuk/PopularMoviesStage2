1fc91dc3ff0f5668a40b9ef21c7ea0e603dd55e5
Dominik Neukamm <dominik.neukamm@mbconnectline.de>
2017-09-17 21:07:49 +0200

    add: MovieDbHelper

---
+++ app/src/main/java/com/example/android/popularmovies/data/MovieDbHelper.java
@@ -0,0 +1,39 @@
+package com.example.android.popularmovies.data;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+        private static final String DATABASE_NAME = "movies.db";
+        private static final int DATABASE_VERSION = 1;
+
+
+        public MovieDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            final String CREATE_MOVIES_TABLE =
+                    "CREATE TABLE " + MovieContract.MovieEntry.TABLE_NAME + "("
+                            + MovieContract.MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            + MovieContract.MovieEntry.COLUMN_MOVIE_ID + " TEXT NOT NULL, "
+                            + MovieContract.MovieEntry.COLUMN_MOVIE_TITLE + " TEXT NOT NULL, "
+                            + MovieContract.MovieEntry.COLUMN_MOVIE_USER_RATING + " TEXT NUT NULL DEFAULT 0, "
+                            + MovieContract.MovieEntry.COLUMN_MOVIE_RELEASE_DATE + " TEXT NOT NULL, "
+                            + MovieContract.MovieEntry.COLUMN_MOVIE_PLOT_SYNOPSIS + " TEXT NOT NULL, "
+                            + MovieContract.MovieEntry.COLUMN_MOVIE_POSTER_URI + " TEXT NOT NULL)";
+
+            db.execSQL(CREATE_MOVIES_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + MovieContract.MovieEntry.TABLE_NAME);
+            onCreate(db);
+        }
+}
