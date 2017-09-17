78612f783f68d7e02799fa791165b266b54a6ee9
Dominik Neukamm <dominik.neukamm@mbconnectline.de>
2017-09-18 01:26:50 +0200

    add: MovieLoader and Trailer

---
+++ app/src/main/java/com/example/android/popularmovies/MovieLoader.java
@@ -0,0 +1,56 @@
+package com.example.android.popularmovies;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.example.android.popularmovies.utilities.MovieApiUtils;
+import com.example.android.popularmovies.utilities.NetworkUtils;
+
+import java.net.URL;
+import java.util.List;
+
+public class MovieLoader extends AsyncTaskLoader<List<Movie>> {
+
+    private static final String TAG = MovieLoader.class.getSimpleName();
+
+    private List<Movie> movies;
+    private String sorting;
+
+    public MovieLoader(Context ctx, String sorting) {
+        super(ctx);
+        this.sorting = sorting;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        if (movies == null) {
+            forceLoad();
+        }
+    }
+
+    @Override
+    public List<Movie> loadInBackground() {
+
+        if (sorting == null) {
+            sorting = NetworkUtils.SORTING_POPULAR;
+        }
+
+        URL apiUrl = NetworkUtils.buildUrl(sorting);
+
+        try {
+            String jsonResponse = NetworkUtils.getResponseFromHttpUrl(apiUrl);
+            return MovieApiUtils.getListOfMoviesFromJson(jsonResponse);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    @Override
+    public void deliverResult(List<Movie> data) {
+        movies = data;
+        super.deliverResult(data);
+    }
+
+}
---
+++ app/src/main/java/com/example/android/popularmovies/Trailer.java
@@ -0,0 +1,64 @@
+package com.example.android.popularmovies;
+
+import com.example.android.popularmovies.utilities.NetworkUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Trailer {
+    private String id;
+    private String key;
+    private String name;
+    private String site;
+    private String type;
+
+    private String videoUrl;
+    private String imageUrl;
+
+    public Trailer(String id, String key, String name, String site, String type) {
+        this.id = id;
+        this.key = key;
+        this.name = name;
+        this.site = site;
+        this.type = type;
+
+        this.videoUrl = NetworkUtils.buildYoutubeVideoUrl(this.key);
+        this.imageUrl = NetworkUtils.buildYoutubeImageUrl(this.key);
+    }
+
+    public Trailer(String video, String image) {
+        this.videoUrl = video;
+        this.imageUrl = image;
+    }
+
+    private Trailer(JSONObject trailer){
+        try {
+            this.id = trailer.getString("id");
+            this.key = trailer.getString("key");
+            this.name = trailer.getString("name");
+            this.site = trailer.getString("site");
+            this.type = trailer.getString("type");
+
+            this.videoUrl = NetworkUtils.buildYoutubeVideoUrl(this.key);
+            this.imageUrl = NetworkUtils.buildYoutubeImageUrl(this.key);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
